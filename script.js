28f64e234c8a8eb25030639879063ef383123a67
artur893 <arthal893@gmail.com>
2022-07-16 12:06:16 +0200

    Players now can put sign on the gameboard

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
+// GAMEBOARD
+
 const Gameboard = (function () {
-    const gameBoard = ["x","x","o","o","o","x","x","o","x"]
-    return { gameBoard }
+    const gameBoard = ["", "", "", "", "", "", "", "", ""]
+
+    const getSign = function (round) {
+        if (round % 2 === 0) {
+            sign = playerX.sign
+        }
+        if (round % 2 === 1) {
+            sign = playerO.sign
+        } return sign
+    }
+
+    const putSign = function (index, sign) {
+        gameBoard[index] = sign
+    }
+
+    return { gameBoard, putSign, getSign }
 })()
 
+// DOM MANIMULATE
+
+const Dom = (function () {
+    const square = document.querySelectorAll(".square")
+
+    return { square }
+})()
+
+//RENDER
 
 const Render = (function () {
-
+    const render = function () {
+        for (i = 0; i < Gameboard.gameBoard.length; i++) {
+            Dom.square[i].textContent = Gameboard.gameBoard[i]
+        }
+    }
+    return { render }
 })()
 
-const Gameflow = (function(){
-
-})()
+//PLAYERS
 
 const player = function (name, sign) {
     return { name, sign }
@@ -19,6 +47,25 @@ const player = function (name, sign) {
 const playerX = player("playerX", "X")
 const playerO = player("playerO", "O")
 
+//GAMEFLOW
 
-console.log(gameBoard)
+const Gameflow = (function () {
+    let round = 1
+    const playGame = function () {
+        Dom.square.forEach((square) => {
+            square.addEventListener("click", function (e) {
+                const index = e.target.dataset.index
+                round++
+                sign = Gameboard.getSign(round)
+                Gameboard.putSign(index, sign)
+                Render.render()
+            })
+        })
+    }
+    return { playGame }
+})()
+
+
+Gameflow.playGame()
+
 
