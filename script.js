67cb7997385883a6b6d136da2c90013c514e10dd
artur893 <arthal893@gmail.com>
2022-07-19 11:04:34 +0200

    Reset button works now

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // GAMEBOARD
 
 const Gameboard = (function () {
-    const gameBoard = ["", "", "", "", "", "", "", "", ""]
+    let gameBoard = ["", "", "", "", "", "", "", "", ""]
 
     const getSign = function (round) {
         if (round % 2 === 0) {
@@ -18,15 +18,52 @@ const Gameboard = (function () {
         }
     }
 
-    return { gameBoard, putSign, getSign }
+    const reset = function () {
+        for (i = 0; i < gameBoard.length; i++) {
+            gameBoard[i] = ""
+        }
+    }
+
+    const winCheck = function () {
+        const winCheckO = function (a, b, c) {
+            if (gameBoard[a] === playerO.sign && gameBoard[b] === playerO.sign && gameBoard[c] === playerO.sign) {
+                console.log("YEAH OOOOOOO!")
+            }
+        }
+        winCheckO(0, 1, 2)
+        winCheckO(3, 4, 5)
+        winCheckO(6, 7, 8)
+        winCheckO(0, 3, 6)
+        winCheckO(1, 4, 7)
+        winCheckO(2, 5, 8)
+        winCheckO(0, 4, 8)
+        winCheckO(2, 4, 6)
+
+        const winCheckX = function (a, b, c) {
+            if (gameBoard[a] === playerX.sign && gameBoard[b] === playerX.sign && gameBoard[c] === playerX.sign) {
+                console.log("YEAH XXXXXX!")
+            }
+        }
+        winCheckX(0, 1, 2)
+        winCheckX(3, 4, 5)
+        winCheckX(6, 7, 8)
+        winCheckX(0, 3, 6)
+        winCheckX(1, 4, 7)
+        winCheckX(2, 5, 8)
+        winCheckX(0, 4, 8)
+        winCheckX(2, 4, 6)
+    }
+
+    return { gameBoard, putSign, getSign, winCheck, reset }
 })()
 
 // DOM MANIMULATE
 
 const Dom = (function () {
     const square = document.querySelectorAll(".square")
+    const newgame = document.querySelector(".newgame")
 
-    return { square }
+    return { square, newgame }
 })()
 
 //RENDER
@@ -54,18 +91,31 @@ const playerO = player("playerO", "O")
 const Gameflow = (function () {
     let round = 1
     const playGame = function () {
+        resetBoard()
         Dom.square.forEach((square) => {
             square.addEventListener("click", function (e) {
                 const index = e.target.dataset.index
                 if (Gameboard.gameBoard[index] === "") {
                     round++
+                    console.log(index)
                     Gameboard.putSign(index, Gameboard.getSign(round))
                     Render.render()
+                    Gameboard.winCheck()
                 }
 
             })
+
         })
     }
+    const resetBoard = function () {
+        Dom.newgame.addEventListener("click", function () {
+            round = 1
+            Gameboard.reset()
+            Render.render()
+        })
+    
+    }
+
     return { playGame }
 })()
 
